aaf74484774a63c0c2ccf42b89618f1261877340
Prasad Mistry <pmistry21@myseneca.ca>
2024-05-24 08:56:00 -0400

    Added lab2a

---
+++ lab2a.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Description: This program will output the name "Jon", as well as their age.
+name = 'Jon'
+age = 20
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')

affbdd3613dfdcb80c4d46b7233908fac6f14e7e
Prasad Mistry <pmistry21@myseneca.ca>
2024-05-24 16:06:43 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: Prasad Mistry
+# Author ID: pmistry21
+# Date Created: 2024/05/24
+
+# Description: This script will countdown a rocket taking off.
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")

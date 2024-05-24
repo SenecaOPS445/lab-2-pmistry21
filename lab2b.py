649f69e1729399b296e1608c8c23d9e1f7e9d4ee
Prasad Mistry <pmistry21@myseneca.ca>
2024-05-24 09:03:55 -0400

    Added lab2b

---
+++ lab2b.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Description: This program will output the user's name, as well as their age.
+name = input("Name: ")
+age = input ("Age: ")
+print('Hi ' + str(name) + ', you are ' + str(age) + ' years old.')

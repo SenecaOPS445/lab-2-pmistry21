a2ce46f772ee49a6c7e3ca6bcfe9e6fe26cd04de
Prasad Mistry <pmistry21@myseneca.ca>
2024-05-24 10:47:35 -0400

    Added lab2d

---
+++ lab2d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Description: This program will use sys to output the user's name, as well as their age.
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: " + sys.argv[0] + " name age")
+    sys.exit()
+
+name = sys.argv[1]
+age = int(sys.argv[2])
+
+print("Hi " + str(name) + ", you are " + str(age) + " years old.")
